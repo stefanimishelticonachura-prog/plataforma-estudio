a2425d2371b38e6817d0e59ac5effdfdb6b67a75
stefanimishelticonachura-prog <stefanimishelticonachura@gmail.com>
2026-08-10 04:03:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -267,7 +267,6 @@ if (isset($_COOKIE['remember_correo'])) {
                         <input type="checkbox" name="remember" id="remember" <?php echo $remember_checked ? 'checked' : ''; ?>>
                         Recordarme
                     </label>
-                    <a href="recuperacion.php"><i class="fas fa-question-circle"></i> ¿Olvidaste tu contraseña?</a>
                 </div>
 
                 <button type="submit" class="btn-login">
